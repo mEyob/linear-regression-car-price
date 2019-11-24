99d42d60b030418f6880fe85a8ff9dc9fdcbeaf7
Misikir <mikireyob@gmail.com>
2019-11-24 10:17:28 -0500

    validate trim level

--- serverless_api/sls-api/app.py
+++ serverless_api/sls-api/app.py
@@ -25,28 +25,39 @@ def predict():
     trim = request.form.get('trim')
     trim = trim.lower()
     mileage = request.form.get('mileage')
+    trim_text = '==='
     # features = [x for x in request.form.values()]
     if car_model == 'camry':
+        if trim not in ['le', 'se', 'xle', 'xse', 'hybrid']:
+            trim_text = 'Trim level not recognised. Base level assumed.'
         trim_level = [1 if entry else 0 for entry in [trim == 'xle', trim == 'xse', trim == 'hybrid']]
         prediction = toyota_model.coef_.dot(np.array([np.log2(int(mileage)), 2019 - int(year), *trim_level])) + toyota_model.intercept_
         prediction_interval = toyota_model.pred_interval
         car_summary = '{} Toyota Camry {}, Mileage - {}'.format(year, trim.upper(), mileage)
     elif car_model == 'accord':
+        if trim not in ['lx', 'ex', 'exl', 'sport', 'touring']:
+            trim_text = 'Trim level not recognised. Base level assumed.'
         trim_level = [1 if entry else 0 for entry in [trim == 'ex', trim == 'exl', trim == 'sport', trim == 'touring']]
         prediction = honda_model.coef_.dot(np.array([np.log2(int(mileage)), 2019 - int(year), *trim_level])) + honda_model.intercept_
         prediction_interval = honda_model.pred_interval
         car_summary = '{} Honda Accord {}, Mileage - {}'.format(year, trim, mileage)
     elif car_model == 'altima':
+        if trim not in [ 'sr', 'sv', 'sl', 's']:
+            trim_text = 'Trim level not recognised. Base level assumed.'
         trim_level = [1 if entry else 0 for entry in [trim == 'sr', trim == 'sv', trim == 'sl']]
         prediction = nissan_model.coef_.dot(np.array([np.log2(int(mileage)), 2019 - int(year), *trim_level])) + nissan_model.intercept_
         prediction_interval = nissan_model.pred_interval
         car_summary = '{} Nissan Altima {}, Mileage - {}'.format(year, trim, mileage)
     elif car_model == 'sonata':
+        if trim not in ['sel', 'sport', 'limited', 'eco', 'se']:
+            trim_text = 'Trim level not recognised. Base level assumed.'
         trim_level = [1 if entry else 0 for entry in [trim ==  'sel', trim == 'sport', trim == 'limited']]
         prediction = hyundai_model.coef_.dot(np.array([np.log2(int(mileage)), 2019 - int(year), *trim_level])) + hyundai_model.intercept_
         prediction_interval = hyundai_model.pred_interval
         car_summary = '{} Hyundai Sonata {}, Mileage - {}'.format(year, trim, mileage)
     elif car_model == 'fusion':
+        if trim not in ['se', 'sel', 'titanium', 'sport', 'platinum']:
+            trim_text = 'Trim level not recognised. Base level assumed.'
         trim_level = [1 if entry else 0 for entry in [trim == 'se', trim == 'sel', trim == 'titanium', trim == 'sport']]
         prediction = ford_model.coef_.dot(np.array([np.log2(int(mileage)), 2019 - int(year), *trim_level])) + ford_model.intercept_
         prediction_interval = ford_model.pred_interval
@@ -57,6 +68,7 @@ def predict():
     return render_template(
         'index.html', 
         summary_text='You Entered:\n\t{}'.format(car_summary),
+        trim_text=trim_text,
         prediction_text='Estimated average price: ${}'.format(int(prediction)), 
         interval_text='Estimated price range: ${} - ${}'.format(int(lower), int(upper))
         )
